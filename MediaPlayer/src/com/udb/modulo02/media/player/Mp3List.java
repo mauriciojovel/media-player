4c707fbb49288477d4c015d75c500ede0114aa15
mauriciojovel <mauriciojovel@gmail.com>
2013-08-30 18:16:45 -0600

    - Se agrego el directorio de sdcard

--- MediaPlayer/src/com/udb/modulo02/media/player/Mp3List.java
+++ MediaPlayer/src/com/udb/modulo02/media/player/Mp3List.java
@@ -2,12 +2,14 @@ package com.udb.modulo02.media.player;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class Mp3List extends ActionBarActivity {
-    private String path = "/";
+    private String path = Environment
+                    .getExternalStorageDirectory().getAbsolutePath();
 	private int ACTIVITY_LIST_RESULT = 10;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
